0e3b9d7cfbc398613221c61bed30005689852eb6
“jooyoung” <jooyoung@pleaze.kr>
2022-06-05 09:48:49 +0900

    택스트 정리

---
+++ lib/utils/textFrame.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+import './colors.dart';
+
+class CommonText {
+  static TextStyle TitleL = TextStyle(
+    fontSize: 24.0,
+    color: Colors.black,
+    fontFamily: 'NotoSansKR_Bold',
+  );
+  static TextStyle TitleLargeWhite = TextStyle(
+    fontSize: 24.0,
+    color: Colors.white,
+    fontFamily: 'NotoSansKR_Bold',
+  );
+  static TextStyle TitleLargeMain = TextStyle(
+    fontSize: 24.0,
+    color: Palette.main,
+    fontFamily: 'NotoSansKR_Bold',
+  );
+
+  static TextStyle TitleM = TextStyle(
+    fontSize: 20.0,
+    color: Colors.black,
+    fontFamily: 'NotoSansKR_Bold',
+  );
+  static TextStyle TitleMediumWhite = TextStyle(
+    fontSize: 20.0,
+       color: Colors.white,
+    fontFamily: 'NotoSansKR_Bold',
+  );
+  static TextStyle TitleMediumMain = TextStyle(
+    fontSize: 20.0,
+    color: Palette.main,
+    fontFamily: 'NotoSansKR_Bold',
+  );
+
+  static TextStyle TitleS = TextStyle(
+    fontSize: 16.0,
+    color: Colors.black,
+    fontFamily: 'NotoSansKR_Bold',
+  );
+  static TextStyle TitleSmallWhite = TextStyle(
+    fontSize: 16.0,
+    color: Colors.white,
+    fontFamily: 'NotoSansKR_Bold',
+  );
+  static TextStyle TitleSmallMain = TextStyle(
+    fontSize: 16.0,
+    color: Palette.main,
+    fontFamily: 'NotoSansKR_Bold',
+  );
+
+  static TextStyle BodyL = TextStyle(
+    fontSize: 14.0,
+    color: Colors.black,
+    fontFamily: 'NotoSansKR_Bold',
+  );
+  static TextStyle BodyLargeWhite = TextStyle(
+    fontSize: 14.0,
+    color: Colors.black,
+    fontFamily: 'NotoSansKR_Bold',
+  );
+  static TextStyle BodyLargeMain = TextStyle(
+    fontSize: 14.0,
+    color: Colors.black,
+    fontFamily: 'NotoSansKR_Bold',
+  );
+
+  static TextStyle BodyB = TextStyle(
+    fontSize: 12.0,
+    color: Colors.black,
+    fontFamily: 'NotoSansKR_Bold',
+  );
+  static TextStyle BodyBoldWhite = TextStyle(
+    fontSize: 12.0,
+    color: Colors.white,
+    fontFamily: 'NotoSansKR_Bold',
+  );
+  static TextStyle BodyBoldMain = TextStyle(
+    fontSize: 12.0,
+    color: Palette.main,
+    fontFamily: 'NotoSansKR_Bold',
+  );
+
+  static TextStyle BodyM = TextStyle(
+    fontSize: 12.0,
+    color: Colors.black,
+    fontFamily: 'NotoSansKR_Md',
+  );
+  static TextStyle BodyMediumWhite = TextStyle(
+    fontSize: 12.0,
+    color: Colors.white,
+    fontFamily: 'NotoSansKR_Md',
+  );
+  static TextStyle BodyMediumMain = TextStyle(
+    fontSize: 12.0,
+    color: Palette.main,
+    fontFamily: 'NotoSansKR_Md',
+  );
+
+  static TextStyle BodyS = TextStyle(
+    fontSize: 12.0,
+    color: Colors.black,
+    fontFamily: 'NotoSansKR_Rg',
+  );
+  static TextStyle BodySmallWhite = TextStyle(
+    fontSize: 12.0,
+    color: Colors.white,
+    fontFamily: 'NotoSansKR_Rg',
+  );
+  static TextStyle BodySmallMain = TextStyle(
+    fontSize: 12.0,
+    color: Palette.main,
+    fontFamily: 'NotoSansKR_Rg',
+  );
+
+  static TextStyle BodyXS = TextStyle(
+    fontSize: 10.0,
+    color: Colors.black,
+    fontFamily: 'NotoSansKR_Rg',
+  );
+  static TextStyle BodyXSmallWhite = TextStyle(
+    fontSize: 10.0,
+    color: Colors.white,
+    fontFamily: 'NotoSansKR_Rg',
+  );
+  static TextStyle BodyXSmallMain = TextStyle(
+    fontSize: 10.0,
+    color: Palette.main,
+    fontFamily: 'NotoSansKR_Rg',
+  );
+
+  static TextStyle BodyEng = TextStyle(
+    fontSize: 10.0,
+    color: Colors.black,
+    fontFamily: 'Roboto_Rg',
+  );
+  static TextStyle BodyEngWhite = TextStyle(
+    fontSize: 10.0,
+    color: Colors.white,
+    fontFamily: 'Roboto_Rg',
+  );
+  static TextStyle BodyEngMain = TextStyle(
+    fontSize: 10.0,
+    color: Palette.main,
+    fontFamily: 'Roboto_Rg',
+  );
+
+  static TextStyle Label = TextStyle(
+    fontSize: 10.0,
+    color: Colors.black,
+    fontFamily: 'NotoSansKR_Md',
+  );
+  static TextStyle LabelWhite = TextStyle(
+    fontSize: 10.0,
+    color: Colors.white,
+    fontFamily: 'NotoSansKR_Md',
+  );
+  static TextStyle LabelMain = TextStyle(
+    fontSize: 10.0,
+    color: Palette.main,
+    fontFamily: 'NotoSansKR_Md',
+  );
+
+  static TextStyle Number = TextStyle(
+    fontSize: 12.0,
+    color: Colors.black,
+    fontFamily: 'Roboto_Md',
+  );
+  static TextStyle NumberWhite = TextStyle(
+    fontSize: 12.0,
+    color: Colors.white,
+    fontFamily: 'Roboto_Md',
+  );
+  static TextStyle NumberMain = TextStyle(
+    fontSize: 12.0,
+    color: Palette.main,
+    fontFamily: 'Roboto_Md',
+  );
+
+  static TextStyle Chip = TextStyle(
+    fontSize: 8.0,
+    color: Colors.black,
+    fontFamily: 'Roboto_Rg',
+  );
+  static TextStyle ChipWhite = TextStyle(
+    fontSize: 8.0,
+    color: Colors.white,
+    fontFamily: 'Roboto_Rg',
+  );
+  static TextStyle ChipMain = TextStyle(
+    fontSize: 8.0,
+    color: Palette.main,
+    fontFamily: 'Roboto_Rg',
+  );
+}
