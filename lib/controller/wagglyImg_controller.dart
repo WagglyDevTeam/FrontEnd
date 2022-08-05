b22d466345303431a1dac4104a40ec686f890a29
59 <5.9.devv@gmail.com>
2022-08-05 22:54:09 +0900

    update code

--- lib/controller/wagglyImg_controller.dart
+++ lib/controller/wagglyImg_controller.dart
@@ -4,6 +4,8 @@ import 'package:wagly/sever/server.dart';
 
 class WagglyImgController extends GetxController {
   var wagglyImglist = <WagglyImg>[].obs;
+  var imgList = <WagglyImgModel>[].obs;
+  int checkedImgNumber = 0;
 
   @override
   void onInit() {
@@ -16,6 +18,19 @@ class WagglyImgController extends GetxController {
     print(wagglyImgs);
     if (wagglyImgs != null) {
       wagglyImglist.value = wagglyImgs;
+    }
+  }
+
+  void getImg() async {
+    imgList.value = <WagglyImgModel>[];
+    for (var i = 0; i < wagglyImglist.length; i++) {
+      var imgItem = wagglyImglist[i];
+      imgList.add(
+        WagglyImgModel(
+          id: imgItem.id,
+          img: imgItem.img,
+        ),
+      );
     }
   }
 }
