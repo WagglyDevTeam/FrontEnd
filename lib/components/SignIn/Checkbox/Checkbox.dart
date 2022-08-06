273b6c71b03c8c173fe48d08fd3d9391f16b4c43
우민기 <woomingy@umingiui-MacBookPro.local>
2022-08-06 21:11:31 +0900

    signup page

---
+++ lib/components/SignIn/Checkbox/Checkbox.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:wagly/utils/textFrame.dart';
+
+class CustomCheckbox extends StatefulWidget {
+  const CustomCheckbox({Key? key}) : super(key: key);
+
+  @override
+  State<CustomCheckbox> createState() => _CustomCheckboxState();
+}
+
+class _CustomCheckboxState extends State<CustomCheckbox> {
+  var isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(12, 0, 18, 0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Checkbox(
+                  activeColor: Color.fromRGBO(184, 99, 251, 1),
+                  value: isChecked,
+                  onChanged: (value) {
+                    setState(() {
+                      isChecked = value!;
+                    });
+                  }),
+              Column(
+                children: [
+                  Text('자동로그인',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 12,
+                      )),
+                  SizedBox(
+                    height: 3,
+                  )
+                ],
+              )
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+        ],
+      ),
+    );
+  }
+}
