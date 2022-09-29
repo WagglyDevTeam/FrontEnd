7d2f379c521953845e1bbe17065335c9f33c3b46
keimindev <keimindev@gmail.com>
2022-09-29 23:16:28 +0900

    [feat/mypage] 마이페이지_라우팅시 자기소개글 버튼 초기화

--- lib/widgets/header/page_appbar.dart
+++ lib/widgets/header/page_appbar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:waggly/components/post/post_modal.dart';
+import 'package:waggly/controller/myPage/my_profile_controller.dart';
 import 'package:waggly/utils/text_frame.dart';
 import 'package:waggly/widgets/index.dart';
 import 'package:waggly/widgets/sign_in.dart';
@@ -57,6 +58,7 @@ class PageAppbar extends StatelessWidget implements PreferredSizeWidget {
     switch (page) {
       case Status.main:
       case Status.home:
+      case Status.editAlarmOnly:
         return Row(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -366,6 +368,7 @@ class AlarmBtn extends StatelessWidget {
       onTap: () async {
         // 알림 페이지로 이동
         await Get.put(NotificationController()).getNotification();
+        await Get.put(MyProfileController()).removeData();
         Get.toNamed('/notification');
       },
       child: Container(
