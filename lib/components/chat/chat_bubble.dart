dfeb2b9765e0746b1a0edfb66ca383bf7f52ad2c
GHOH <ghoh@mediance.co.kr>
2022-10-04 13:25:43 +0900

    신고 버튼 만드는중

--- lib/components/chat/chat_bubble.dart
+++ lib/components/chat/chat_bubble.dart
@@ -28,6 +28,8 @@ class ChatBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> reasonList = ["욕설/비방", "부적절 컨텐츠", "판매/사기"];
+
     return Column(
       crossAxisAlignment: isMyMessage ? CrossAxisAlignment.end : CrossAxisAlignment.start,
       children: [
@@ -86,18 +88,104 @@ class ChatBubble extends StatelessWidget {
                   ],
                 ),
               ),
-            Container(
-              constraints: BoxConstraints(maxWidth: 200.w),
-              decoration: BoxDecoration(
-                color: isMyMessage ? Palette.main : Palette.paper,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              // width: 200.w,
-              padding: EdgeInsets.fromLTRB(16.0.w, 10.0.h, 16.0.w, 10.0.h),
-              child: Text(
-                message,
-                style: TextStyle(
-                  color: isMyMessage ? Colors.white : Colors.black,
+            InkWell(
+              // onLongPress: () {
+              onTap: () {
+                print("확인");
+                showModalBottomSheet(
+                  //드래그 안되게~
+                  enableDrag: false,
+                  //modal 위치 외에 클릭 안되게 하는것
+                  isDismissible: true,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                  ),
+                  context: context,
+                  builder: ((context) {
+                    return Container(
+                      padding: EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text('${user.nickName}님을 신고하시겠습니까?'),
+                          SizedBox(height: 5.h),
+                          Divider(thickness: 1, color: Palette.candy),
+                          SizedBox(height: 10.h),
+                          Container(
+                            alignment: Alignment.center,
+                            width: 120,
+                            child: DropdownButtonFormField(
+                              // decoration: InputDecoration(
+                              //   border: OutlineInputBorder(),
+                              // ),
+                              hint: Center(child: Text("신고 사유 선택")),
+                              items: reasonList.map((e) => DropdownMenuItem(child: Text(e), value: e)).toList(),
+                              onChanged: (value){
+                                print(value);
+                              },
+                            ),
+                          ),
+                          SizedBox(height: 20.h),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: EdgeInsets.only(left: 10.0.w, right: 5.0.w),
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    height: 28.0.h,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(26),
+                                      color: Palette.candy,
+                                    ),
+                                    child: Text(
+                                      "취소하기",
+                                      style: CommonText.BodyM,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: EdgeInsets.only(right: 10.0.w, left: 5.0.w),
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    height: 28.0.h,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(26),
+                                      color: Palette.candy,
+                                    ),
+                                    child: Text(
+                                      "신고하기",
+                                      style: CommonText.BodyM,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 20.h),
+                        ],
+                      ),
+                    );
+                  }),
+                );
+              },
+              child: Container(
+                constraints: BoxConstraints(maxWidth: 200.w),
+                decoration: BoxDecoration(
+                  color: isMyMessage ? Palette.main : Palette.paper,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                // width: 200.w,
+                padding: EdgeInsets.fromLTRB(16.0.w, 10.0.h, 16.0.w, 10.0.h),
+                child: Text(
+                  message,
+                  style: TextStyle(
+                    color: isMyMessage ? Colors.white : Colors.black,
+                  ),
                 ),
               ),
             ),
