9fa6b20768813a7b7e2ae5663aa1610a9fb67583
59 <5.9.devv@gmail.com>
2023-01-16 17:24:04 +0900

    tmp

---
+++ lib/controller/pushNotification/app_controller.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:get/get.dart';
+import 'package:waggly/firebase_options.dart';
+
+class AppController extends GetxController {
+  static AppController get to => Get.find();
+
+  final Rxn<RemoteMessage> message = Rxn<RemoteMessage>();
+
+  Future<bool> initialize() async {
+    // Firebase 초기화부터 해야 Firebase Messaging 을 사용할 수 있다.
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    // Android 에서는 별도의 확인 없이 리턴되지만, requestPermission()을 호출하지 않으면 수신되지 않는다.
+    await FirebaseMessaging.instance.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true, // Required to display a heads up notification
+      badge: true,
+      sound: true,
+    );
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage rm) {
+      message.value = rm;
+    });
+
+    return true;
+  }
+
+  Future<String> getToken() async {
+    final String? token = await FirebaseMessaging.instance.getToken();
+    print('token is $token!');
+    return token!;
+  }
+}
