08e35c4e690f36c896c7d1d50d1a9fe07e9c7ec1
keimindev <keimindev@gmail.com>
2023-04-24 22:04:30 +0900

    [feature/home] 홈_유저 해당과 베스트 글 가져오기 수정

--- lib/provider/home_provider.dart
+++ lib/provider/home_provider.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
 import 'package:waggly/controller/signIn/sign_in_conroller.dart';
+import 'package:waggly/hive/user.dart';
 
 final SignInController _signInController = Get.put(SignInController());
 final _token = _signInController.getToken();
@@ -8,8 +10,9 @@ final _token = _signInController.getToken();
 class HomeProvider extends GetConnect {
   final Map<String, String> authHeaders = _token != null ? {"Authorization": _token!} : {};
 
+  final userId = Hive.box<User>('user').get('user')?.id;
   Future<Response> getHome() => get(
-        "${dotenv.get('BASE_URL')}/home",
+        "${dotenv.get('BASE_URL')}/home?userId=$userId",
         headers: authHeaders,
       );
 }
